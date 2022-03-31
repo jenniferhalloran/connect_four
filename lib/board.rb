df63ec76f87dd03478b81bf8d1aa960569f0b15e
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-31 15:50:42 -0400

    Refactor board class and tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,25 @@
+
 class Board
- attr_reader :rows
+ attr_reader :rows, :column_a,:column_b,:column_c,:column_d,:column_e,:column_f,:column_g
 
   def initialize
-    @rows = [
-      ['.','.','.','.','.','.','.'],
-      ['.','.','.','.','.','.','.'],
-      ['.','.','.','.','.','.','.'],
-      ['.','.','.','.','.','.','.'],
-      ['.','.','.','.','.','.','.'],
-      ['.','.','.','.','.','.','.']
+    @columns = [
+      ['.','.','.','.','.','.'], # = column A = @columns[0]
+      ['.','.','.','.','.','.'], # = column B = @columns[1]
+      ['.','.','.','.','.','.'], # = column C = @columns[2]
+      ['.','.','.','.','.','.'], # = column D = @columns[3]
+      ['.','.','.','.','.','.'], # = column E = @columns[4]
+      ['.','.','.','.','.','.'], # = column F = @columns[5]
+      ['.','.','.','.','.','.']  # = column G = @columns[6]
     ]
-    # @columns = [a, b, c, d, e, f, g]
+    @column_a = @columns[0]
+    @column_b = @columns[1]
+    @column_c = @columns[2]
+    @column_d = @columns[3]
+    @column_e = @columns[4]
+    @column_f = @columns[5]
+    @column_g = @columns[6]
+
   end
 
   def welcome_message
@@ -19,15 +28,20 @@ class Board
   end
 
   def print_board
-    # @rows = rows
     puts " "
     puts "A B C D E F G"
-    # require "pry"; binding.pry
-    board = @rows.reverse.map do |row|
-      puts row.join(' ')
-      end
+    @columns.transpose.reverse.map do |column|
+      puts column.join(' ')
+    end
     puts " "
     #The puts " " at the start and end aren't really necessary, was just doing it for visual purposes
   end
 
 end
+
+board = Board.new
+board.print_board
+
+# p board.column_a
+
+# require "pry"; binding.pry
