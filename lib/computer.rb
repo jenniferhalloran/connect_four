6672f5e6eb27734a6e90dacedb36aa02626ccbdd
“Brad <hutch13531@gmail.com>
2022-04-03 19:21:52 -0400

    Features: Valid_column?, validate_column

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,20 +1,45 @@
 require './lib/board'
 require 'pry'
 
-class Computer #added computer class
+class Computer
 attr_reader :board
   def initialize(board)
     @board = board
   end
 
   def choose_column
-    p @board.columns.sample
-  end #Ramdomly selects a board column
+    column_choice = @board.columns.sample
+    validate_column(column_choice)
+  end
 
+  def valid_column?(column_choice)
+    if column_choice == "A"
+      @board.column_a.include?(".")
+    elsif column_choice == "B"
+      @board.column_b.include?(".")
+    elsif column_choice == "C"
+      @board.column_c.include?(".")
+    elsif column_choice == "D"
+      @board.column_d.include?(".")
+    elsif column_choice == "E"
+      @board.column_e.include?(".")
+    elsif column_choice == "F"
+      @board.column_f.include?(".")
+    elsif column_choice == "G"
+      @board.column_g.include?(".")
+    end
+  end
 
-  def drop(column_choice)#added drop method
-    # https://stackoverflow.com/questions/27465153/how-can-i-replace-one-single-element-in-an-array-in-ruby
-    # this drop method is taking the players input then finding the first index that = "." and replacing it with an x
+  def validate_column(column_choice)
+    if valid_column?(column_choice)
+      puts "The computer chose column #{column_choice}"
+      drop(column_choice)
+    else
+      choose_column
+    end
+  end
+
+  def drop(column_choice)
     if column_choice == "a"
       board.column_a[board.column_a.index(".")] = "o"
     elsif column_choice == "b"
@@ -35,5 +60,6 @@ attr_reader :board
     elsif column_choice == "g"
       board.column_g[board.column_g.index(".")] = "o"
     end
+    @board.print_board
   end
 end
