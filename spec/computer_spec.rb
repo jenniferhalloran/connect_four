454da6b3c177d82e9325d59bfead4a4e31447f2d
“Brad <hutch13531@gmail.com>
2022-04-04 17:18:16 -0400

    Test: can drop an 'o' on top of another 'o'

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Computer do
     board = Board.new
     computer = Computer.new(board)
   expect(computer).to be_an_instance_of(Computer)
-end #Test that the computer exists
+end
 
   it "initializes with a board" do
     board = Board.new
     computer = Computer.new(board)
   expect(computer.board).to eq(board)
-end #Test that the computer has a board
+end
 
   it "can access columns from the board" do
     board = Board.new
@@ -59,8 +59,31 @@ end #Test that the computer has a board
     computer.drop("G")
 
     expect(board.column_g).to eq(['o','o','.','.','.','.'])
-  end # Test the computer can add an "o" to the board
+  end
 
+  it "can drop an 'o' on top of another 'o'" do
+    board = Board.new
+    computer = Computer.new(board)
+    computer.drop("A")
+    computer.drop("B")
+    computer.drop("C")
+    computer.drop("D")
+    computer.drop("E")
+    computer.drop("F")
+    computer.drop("G")
+
+    expect(board.column_a).to eq(['o','.','.','.','.','.'])
+    expect(board.column_b).to eq(['o','.','.','.','.','.'])
+    expect(board.column_c).to eq(['o','.','.','.','.','.'])
+    expect(board.column_d).to eq(['o','.','.','.','.','.'])
+    expect(board.column_e).to eq(['o','.','.','.','.','.'])
+    expect(board.column_f).to eq(['o','.','.','.','.','.'])
+    expect(board.column_g).to eq(['o','.','.','.','.','.'])
+
+    computer.drop("G")
+
+    expect(board.column_g).to eq(['o','o','.','.','.','.'])
+  end
 
 
 
